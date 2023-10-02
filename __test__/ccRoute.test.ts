5b3691391e8800c3557361571b97bc43acd25f59
SFJohnson24 <SFJohnson24@gmail.com>
2023-10-02 15:20:58 -0400

    boilerplate and npm i

---
+++ __test__/ccRoute.test.ts
@@ -0,0 +1,9 @@
+/**
+ * Unit testing for charity controllers
+ *
+ * Test:
+ *  - GET : receive JSON of all instances 
+ *  - GET : receive JSON info of the profile: username (and causes potentially)
+ *  - POST: write a new charity instance 
+ *  - DELETE : deletes a charity instance row
+ */
